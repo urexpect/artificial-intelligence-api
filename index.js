9f9a95563d633e5a92830cb1120d25396048d26b
urexpect <106760141+urexpect@users.noreply.github.com>
2022-06-03 19:11:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var fs=require('fs');
-var data=fs.readFileSync('chemistry.json');
-var elements=JSON.parse(data);
+var tr_tr=fs.readFileSync('./Localization/tr-tr.json');
+var en_en=fs.readFileSync('./Localization/en-en.json');
+var tr_tr_elements=JSON.parse(tr_tr);
+var en_en_elements=JSON.parse(en_en);
 const express = require("express");
 const app = express();
 const cors=require('cors');
 
-app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
+app.listen(process.env.PORT, () => console.log("Server Start at "+process.env.PORT+ " Port"));
 
 app.use(express.static('public'));
 app.use(cors());
-app.get('/elements',alldata);
+app.get('/tr_tr_elements',alldata);
 function alldata(request,response)
 {
     response.send(elements);
 }
-app.get('/elements/:element/',searchElement);
+app.get('/tr_tr_elements/:element/',searchElement);
 function searchElement(request,response)
 {
 	var word=request.params.element;
